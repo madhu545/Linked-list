62947763f18c4a3ee9b00f76043fdae940863160
madhu sudhan <mmudiraj90@gmail.com>
2021-05-20 11:17:50 +0530

    Added:code for BST

---
+++ test/java/MyBinaryTreeTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyBinaryTreeTest {
+
+    @Test
+    public void given3NumbersWhenAddedToABinarySearchTreeShouldReturn3() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(3, size);
+    }
+
+    @Test
+    public void given13NumbersWhenAddedToABinarySearchTreeShouldReturn13() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(13, size);
+    }
+
+    @Test
+    public void givenNodeForSearchingInBSTShouldReturnTrue() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        Assertions.assertTrue(myBinaryTree.search(63));
+    }
+}
